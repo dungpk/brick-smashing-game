90bb4469f5c29b530354b94f92693ba28348082d
User Name~ <dungphung01041998@gmail.com>
2023-03-15 14:18:03 +0700

    handle ball collide paddle

--- game.js
+++ game.js
@@ -1,35 +1,47 @@
 let canvas = document.getElementById('game');
 let context = canvas.getContext('2d');
 
-let x=20,y=20;
-let dx=10,dy=14;
-let radius = 20
+
+let ball= {
+    x : 20,
+    y : 20,
+    dx:5,
+    dy:5,
+    radius:10
+
+};
 
 let paddle = {
     width :70,
     height: 10,
     x:0,
     y:canvas.height - 10,
-    speed: 35
+    speed: 10,
+    isMovingLeft : false,
+    isMovingRight: false,
 };
 
+let isGameOver = false;
+
 document.addEventListener('keydown',function(event){
-    console.log("KEY DOWN")
-    console.log(event);
     if(event.keyCode === 37){
-        paddle.x -= paddle.speed;
+        paddle.isMovingLeft = true;
     }else if(event.keyCode === 39){
-        paddle.x += paddle.speed;
+        paddle.isMovingRight = true;
     }
 });
 document.addEventListener('keyup',function(event){
-    console.log("KEY UP")
-    console.log(event);
+
+    if(event.keyCode === 37){
+        paddle.isMovingLeft = false;
+    }else if(event.keyCode === 39){
+        paddle.isMovingRight = false;
+    }
 });
 function drawBall()
 {
     context.beginPath()
-    context.arc(x, y, radius, 0, Math.PI * 2);
+    context.arc(ball.x,ball.y, ball.radius, 0, Math.PI * 2);
     context.fillStyle = 'red'
     context.fill();
     context.closePath();
@@ -43,24 +55,63 @@ function drawPaddle(){
 }
 
 function handleBallCollideBounds(){
-    if(x<0||x>canvas.width-20){
-        dx = -dx;
+    if(ball.x<ball.radius||ball.x>canvas.width-20){
+        ball.dx = -ball.dx;
     }
-    if(y<0||y>canvas.height-20){
-        dy = -dy;
+    if(ball.y<ball.radius){
+        ball.dy = -ball.dy;
     }
 }
 function updateBallPosition(){
-    x+=dx;y+=dy;
+    ball.x+=ball.dx;ball.y+=ball.dy;
+}
+
+function handleBallCollidePaddle(){
+    if(ball.x+ball.radius >= paddle.x && ball.x+ball.radius<=paddle.x+paddle.width && ball.y+
+        ball.radius >= canvas.height -paddle.height){
+        ball.dy = -ball.dy;
+    }
+}
+
+function updatePaddlePosition(){
+    if (paddle.isMovingRight) {
+        paddle.x += paddle.speed;
+    } else if (paddle.isMovingLeft) {
+        paddle.x -= paddle.speed;
+    }
+
+    if (paddle.x < 0) {
+        paddle.x = 0;
+    } else if (paddle.x > canvas.width - paddle.width) paddle.x = canvas.width - paddle.width;
+}
+
+function checkOverGame(){
+    if(ball.y>canvas.height - ball.radius){
+        isGameOver = true;
+    }
+}
+
+function handleGameOver(){
+    document.write("Game Over")
 }
 
 function draw(){
-    context.clearRect(0,0,canvas.clientWidth,canvas.clientHeight)
-    drawBall();
-    drawPaddle();
-    handleBallCollideBounds()
-    updateBallPosition()
-    requestAnimationFrame(draw);
+
+    if(!isGameOver) {
+        context.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight)
+        drawBall();
+        drawPaddle();
+        updatePaddlePosition();
+        handleBallCollideBounds()
+        handleBallCollidePaddle();
+        updateBallPosition()
+        checkOverGame();
+
+
+        requestAnimationFrame(draw);
+    }else{
+        handleGameOver();
+    }
 }
 
 draw();
