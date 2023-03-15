dc0616cf713026bcf82665deb38f91d5d678a17b
User Name~ <dungphung01041998@gmail.com>
2023-03-15 11:46:36 +0700

    drawing and remove the bar

--- game.js
+++ game.js
@@ -3,27 +3,63 @@ let context = canvas.getContext('2d');
 
 let x=20,y=20;
 let dx=10,dy=14;
+let radius = 20
+
+let paddle = {
+    width :70,
+    height: 10,
+    x:0,
+    y:canvas.height - 10,
+    speed: 35
+};
+
+document.addEventListener('keydown',function(event){
+    console.log("KEY DOWN")
+    console.log(event);
+    if(event.keyCode === 37){
+        paddle.x -= paddle.speed;
+    }else if(event.keyCode === 39){
+        paddle.x += paddle.speed;
+    }
+});
+document.addEventListener('keyup',function(event){
+    console.log("KEY UP")
+    console.log(event);
+});
 function drawBall()
 {
     context.beginPath()
-    context.arc(x, y, 20, 0, Math.PI * 2);
+    context.arc(x, y, radius, 0, Math.PI * 2);
     context.fillStyle = 'red'
     context.fill();
     context.closePath();
 }
+function drawPaddle(){
+    context.beginPath()
+    context.rect(paddle.x,paddle.y,paddle.width,paddle.height);
+    context.fillStyle = 'violet'
+    context.fill();
+    context.closePath();
+}
 
+function handleBallCollideBounds(){
+    if(x<0||x>canvas.width-20){
+        dx = -dx;
+    }
+    if(y<0||y>canvas.height-20){
+        dy = -dy;
+    }
+}
+function updateBallPosition(){
+    x+=dx;y+=dy;
+}
 
 function draw(){
     context.clearRect(0,0,canvas.clientWidth,canvas.clientHeight)
     drawBall();
-
-    if(x<0||x>canvas.width){
-        dx = -dx;
-    }
-    if(y<0||y>canvas.height){
-        dy = -dy;
-    }
-    x+=dx;y+=dy;
+    drawPaddle();
+    handleBallCollideBounds()
+    updateBallPosition()
     requestAnimationFrame(draw);
 }
 
